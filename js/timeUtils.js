21e493af505bd4dd7fb985fad6237773e154ac63
Rithwikh Varma <rithwikh.v@gmail.com>
2026-09-21 16:47:31 -0700

    Completed timeUtils.js getScheduleKey() and getFormattedTime() functions

--- js/timeUtils.js
+++ js/timeUtils.js
@@ -8,3 +8,26 @@ export function getCurrentMinutes() {
     const now  = new Date();
     return (now.getHours() * 60) + now.getMinutes();
 }
+
+
+/**
+ * Weekday vs Weekend determines which transit lines are available,
+ * so it's essential to gather the current day.
+ */
+
+
+export function getScheduleKey() {
+    const now = new Date();
+    const day = now.getDay();
+
+
+    if (day == 0 /**sunday*/|| day == 6 /*saturday */){
+        return "weekend-transit-lines";
+    }
+    else return "weekday-transit-lines";
+}
+
+export function getFormattedTime() {
+    const now = new Date();
+    return now.toLocaleTimeString([], {hour : "2-digit", minute : "2-digit"});
+}

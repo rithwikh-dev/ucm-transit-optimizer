cfcdc41abbea8afaae7701feedb7792914dfe508
Rithwikh Varma <rithwikh.v@gmail.com>
2026-09-22 00:50:25 -0700

    Preparing Live Server test of json data import, timeutils and the findBestStop() function

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 // js/app.js
 
-import { findBestStop } from "./transitLogic";
+import { findBestStop } from "./transitLogic.js";
+import { getScheduleKey } from "./timeUtils.js";
 
 async function loadTransitData(){
     try{
         const response = await fetch("../data/transit_schedules.json");
         const data = await response.json();
-        console.log("Schedules loaded successfully!");
+        
+        const testResults = findBestStop(data, getScheduleKey(data), "R St. Village Apartments", 500);
+        console.log("Viable bus routes found: ", testResults);
 
     }
 
-    catch {
-        console.log("Couldn't load schedule data: ", error);
+    catch(error) {
+        console.error("Couldn't load schedule data: ", error);
     }
 
 }
